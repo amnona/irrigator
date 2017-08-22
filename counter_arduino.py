6584ba0d767108a308abc2191c1e34dfc27574f6
amnona <amnonim@gmail.com>
2017-08-22 15:48:00 +0300

    arduino counter

--- counter_arduino.py
+++ counter_arduino.py
@@ -19,7 +19,7 @@ class CounterArduino(Counter):
         except:
             logger.warning('cannot connect to water counter %s' % self.serial_name)
             return self.count
-        command = 'r'+str(self.iopin)
+        command = 'r'+str(self.iopin)+'\n'
         ser.write(command.encode())
         try:
             count = ser.readline()
@@ -33,7 +33,7 @@ class CounterArduino(Counter):
     def clear_count(self):
         '''Set the count to 0'''
         ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
-        command = 'c'+str(self.iopin)
+        command = 'c'+str(self.iopin)+'\n'
         ser.write(command.encode())
         new_count = self.get_count()
         if new_count != 0:
