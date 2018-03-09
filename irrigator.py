ad88309108fd988b646157ef5103376bea5c1c30
amnona <amnonim@gmail.com>
2018-03-09 15:07:46 +0200

    fix detection of serial port out of list

--- irrigator.py
+++ irrigator.py
@@ -17,7 +17,7 @@ fh = logging.FileHandler('spam.log')
 fh.setLevel(logging.DEBUG)
 # create console handler with a higher log level
 ch = logging.StreamHandler()
-ch.setLevel(logging.WARNING)
+ch.setLevel(logging.DEBUG)
 # create formatter and add it to the handlers
 formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 fh.setFormatter(formatter)
