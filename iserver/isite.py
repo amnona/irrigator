967c09a7f74b192bcf89ba683996f783573caab2
amnona <amnonim@gmail.com>
2018-03-16 12:48:01 +0200

    get status

--- iserver/isite.py
+++ iserver/isite.py
@@ -30,9 +30,7 @@ def check_auth(username, password):
 
 def authenticate():
 	"""Sends a 401 response that enables basic auth"""
-	return Response('Could not verify your access level for that URL.\n'
-					'You have to login with proper credentials', 401,
-					{'WWW-Authenticate': 'Basic realm="Login Required"'})
+	return Response('Could not verify your access level for that URL.\nYou have to login with proper credentials', 401, {'WWW-Authenticate': 'Basic realm="Login Required"'})
 
 
 def requires_auth(f):
