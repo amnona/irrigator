4c6b640c131992c1664b421c479825f8a81fb765
amnona <amnonim@gmail.com>
2018-03-18 13:58:15 +0200

    logging

--- irrigator.py
+++ irrigator.py
@@ -2,41 +2,39 @@
 
 import logging
 from logging import getLogger
-import time
+import datetime
+from pkg_resources import resource_filename
+from logging.config import fileConfig
+
 
 import icomputer
 import timers
-from counter_arduino import CounterArduino
 
 logger = getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+log = resource_filename(__package__, 'log.cfg')
+
+# setting False allows other logger to print log.
+fileConfig(log, disable_existing_loggers=False)
+
+
 # logger.addHandler(logging.StreamHandler())
 # logger.setLevel(logging.DEBUG)
 
 # create file handler which logs even debug messages
-fh = logging.FileHandler('spam.log')
-fh.setLevel(logging.DEBUG)
+## fh = logging.FileHandler('spam.log')
+## fh.setLevel(logging.DEBUG)
 # create console handler with a higher log level
-ch = logging.StreamHandler()
-ch.setLevel(logging.DEBUG)
+##ch = logging.StreamHandler()
+##ch.setLevel(logging.DEBUG)
 # create formatter and add it to the handlers
-formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-fh.setFormatter(formatter)
-ch.setFormatter(formatter)
+##formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+##fh.setFormatter(formatter)
+##ch.setFormatter(formatter)
 # add the handlers to the logger
-logger.addHandler(fh)
-logger.addHandler(ch)
-
-import datetime
-zz=timers.next_weekday(datetime.datetime.now(), 4)
-print(zz.date())
-
-# ca = CounterArduino(iopin='2')
-# while True:
-#     b = ca.get_count()
-#     print(b)
-#     time.sleep(1)
-
-
+##logger.addHandler(fh)
+##logger.addHandler(ch)
 
 logger.info('Starting irrigator')
 icomp = icomputer.IComputer()
