46ce8652c9a70e5ea22c80e8e8af9f53586c10e7
amnona <amnonim@gmail.com>
2018-04-10 23:53:03 +0300

    command to close all

--- closeall_numato.py
+++ closeall_numato.py
@@ -10,7 +10,7 @@ icomputer.set_log_level('DEBUG')
 logger.debug('running closeall')
 
 for idx in range(16):
-	cfaucet = icomputer.numatofaucet.NumatoFaucet(relay=idx, name='lala', computer_name='pita', local_computer_name='pata')
+	cfaucet = icomputer.numatofaucet.NumatoFaucet(relay=idx, name='lala', computer_name='pita', local_computer_name='pita')
 	print('closing faucet %s' % idx)
 	res = cfaucet.close()
 	print('closed faucet %s. response %s' % (idx, res))
