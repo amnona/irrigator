9e9aa394bf60bd35ad95490ce6597f273cbeb003
amnona <amnonim@gmail.com>
2022-04-09 21:24:53 +0300

    change to sendinblue email server

--- icomputer/utils.py
+++ icomputer/utils.py
@@ -5,16 +5,30 @@ import smtplib
 logger = logging.getLogger(__name__)
 
 
-def send_email(recipient, subject, body, user='irrigation.computer.amnon@gmail.com', pwd=None):
+def send_email(recipient, subject, body, user='irrigation.computer.amnon@gmail.com', pwd=None, smtp_server='smtp-relay.sendinblue.com', smtp_port=587, smtp_user='sugaroops@yahoo.com'):
+	'''this is for the sendinblue smtp email server
+		for gmail use:
+		smtp_server='smtp.gmail.com'
+	'''
 	if pwd is None:
 		if 'IRRIGATOR_EMAIL_PASSWORD' in os.environ:
 			pwd = os.environ['IRRIGATOR_EMAIL_PASSWORD']
 		else:
 			logger.warning('IRRIGATOR_EMAIL_PASSWORD not in environment variables. please set.')
 			return False
+	if smtp_server is None:
+		if 'IRRIGATOR_EMAIL_SMTP_SERVER' in os.environ:
+			pwd = os.environ['IRRIGATOR_EMAIL_SMTP_SERVER']
+		else:
+			logger.warning('IRRIGATOR_EMAIL_SMTP_SERVER not in environment variables. please set.')
+			return False
+	if smtp_user is None:
+		if 'IRRIGATOR_EMAIL_SMTP_USER' in os.environ:
+			pwd = os.environ['IRRIGATOR_EMAIL_SMTP_USER']
+		else:
+			logger.warning('IRRIGATOR_EMAIL_SMTP_USER not in environment variables. please set.')
+			return False
 
-	gmail_user = user
-	gmail_pwd = pwd
 	FROM = user
 	TO = recipient if type(recipient) is list else [recipient]
 	SUBJECT = subject
@@ -23,10 +37,10 @@ def send_email(recipient, subject, body, user='irrigation.computer.amnon@gmail.c
 	# Prepare actual message
 	message = """From: %s\nTo: %s\nSubject: %s\n\n%s""" % (FROM, ", ".join(TO), SUBJECT, TEXT)
 	try:
-		server = smtplib.SMTP("smtp.gmail.com", 587)
+		server = smtplib.SMTP(smtp_server, smtp_port)
 		server.ehlo()
 		server.starttls()
-		server.login(gmail_user, gmail_pwd)
+		server.login(smtp_user, pwd)
 		server.sendmail(FROM, TO, message)
 		server.close()
 		logger.info('sent email: subject %s to %s' % (SUBJECT, TO))
