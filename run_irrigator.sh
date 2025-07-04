095c466e2c49e9b04fd1c3aa54efbdcaa848fae2
amnona <amnonim@gmail.com>
2025-07-04 16:22:01 +0300

    add secrets file

--- run_irrigator.sh
+++ run_irrigator.sh
@@ -1,7 +1,9 @@
 echo 'run_irrigator.sh' > /home/pi/logs/run_irrigator_out.txt 2> /home/pi/logs/run_irrigator_err.txt
 source /home/pi/miniconda3/bin/activate irrigator >> /home/pi/logs/run_irrigator_out.txt 2>> /home/pi/logs/run_irrigator_err.txt
 cd /home/pi/git/irrigator
-export IRRIGATOR_EMAIL_PASSWORD="hashkaya"
-export IRRIGATOR_PASSWORD="hashkaYa"
+# export the environment variables from secrets.sh
+# (copy from secrets.sh.default and fill in your values)
+source ./secrets.sh >> /home/pi/logs/run_server_out.txt 2>> /home/pi/logs/run_server_err.txt
+
 ./irrigator.py >> /home/pi/logs/run_irrigator_out.txt 2>> /home/pi/logs/run_irrigator_err.txt
 echo 'finished' >> /home/pi/logs/run_irrigator_out.txt 2>> /home/pi/logs/run_irrigator_err.txt
--- run_server.sh
+++ run_server.sh
@@ -1,7 +1,11 @@
 echo 'run_server.sh' > /home/pi/logs/run_server_out.txt 2> /home/pi/logs/run_server_err.txt
 source /home/pi/miniconda3/bin/activate irrigator >> /home/pi/logs/run_irrigator_server.txt 2>> /home/pi/logs/run_server_err.txt
 cd /home/pi/git/irrigator
-export IRRIGATOR_PASSWORD="hashkaYa"
+
+# export the environment variables from secrets.sh
+# (copy from secrets.sh.default and fill in your values)
+source ./secrets.sh >> /home/pi/logs/run_server_out.txt 2>> /home/pi/logs/run_server_err.txt
+
 export FLASK_APP=iserver/iserver.py
 flask run >> /home/pi/logs/run_server_out.txt 2>> /home/pi/logs/run_server_err.txt
 echo 'finished' >> /home/pi/logs/run_server_out.txt 2>> /home/pi/logs/run_server_err.txt
